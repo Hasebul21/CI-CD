09c003046accbb7c14b40e502e2a5bc37dbc9b75
Hasebul Hassan <hasebul@Hasebuls-MacBook-Air.local>
2025-07-16 15:38:11 +0600

    added

---
+++ src/app/karma/karma.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-karma',
+  imports: [],
+  templateUrl: './karma.component.html',
+  styleUrl: './karma.component.css'
+})
+export class KarmaComponent {
+
+}
